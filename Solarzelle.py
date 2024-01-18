e10a5e101a53e873d7560623da885aa43ca3b897
Code-For-Myself <jannissven@gmail.com>
2024-01-18 02:32:35 +0100

    graphen fertig

--- Solarzelle.py
+++ Solarzelle.py
@@ -3,17 +3,50 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
 
+def Auswertung(Pfad, Pfad2):
+    data = pd.read_csv(Pfad, sep=",")
+    data2 = pd.read_csv(Pfad2, sep=",")
+    xdata = np.array(data[" U in V (mess)"])
+    ydata = np.array(data[" I in mA (mess)"])
+    xdata2 = np.array(data2[" U in V (mess)"])
+    ydata2 = np.array(data2[" I in mA (mess)"])
+    xdata2r = xdata2[::-1]
+    ydata2r = ydata2[::-1]
+    xdatazip = np.concatenate((xdata2r, xdata))
+    ydatazip = np.concatenate((ydata2r, ydata))
+    for i in range(len(ydatazip)):
+        if ydatazip[i] < 0:
+            x = i
+    print(x)
+    xlist = np.linspace(min(xdatazip[25:x + 7]), max(xdatazip[25:x + 7]), 1000)
+    popt, pcov = curve_fit(lambda x, a, b, c: a * (np.exp(x / b) - 1) - c, xdatazip[25:x + 7], ydatazip[25:x + 7])
+    y = popt[0] * (np.e ** (xlist / popt[1]) - 1) - popt[2]
+    plt.plot(xlist, y, color="red")
+    plt.scatter(xdatazip[25:x + 7], ydatazip[25:x + 7])
+    print(popt)
+    ifoto = ydatazip[30]
+    for i in range(len(y)):
+        if y[i] < 0:
+            d = i
+    for i in range(len(y)):
+        if xlist[i] < 0:
+            e = i
+    uleer = xlist[d]
+    arr = xlist[e+1:d]*y[e+1:d]
+    pmax = min(arr)
+    sf = 1.12
+    ff = pmax/(ifoto*uleer)
+    cf = sf * ff
+    print("I_Foto: ", ifoto)
+    print("U_L: ", uleer)
+    print("max Leistung: ", pmax)
+    print("Spannungsfaktor: ", sf)
+    print("Füllfaktor: ", ff)
+    print("cFaktor: ", cf)
+    plt.show()
 
-data = pd.read_csv("Grün_525nm_Durchlassrichtung.txt", sep=",")
-data2 = pd.read_csv("Grün_525nm_Sperrrichtung.txt", sep=",")
-xdata = data[" U in V (mess)"]
-ydata = data[" I in mA (mess)"]
-xdata2 = data2[" U in V (mess)"]
-ydata2 = data2[" I in mA (mess)"]
-xdatac = np.concatenate((xdata, xdata2))
-ydatac = np.concatenate((ydata, ydata2))
-xdatazip = list(zip(xdata, xdata2))
-ydatazip = list(zip(abs(ydata), abs(ydata2)))
+Auswertung("Grün_525nm_Durchlassrichtung.txt","Grün_525nm_Sperrrichtung.txt")
+
 def Plotter(Pfad, Pfad2, Label, Marker):
     data = pd.read_csv(Pfad, sep=",")
     data2 = pd.read_csv(Pfad2, sep=",")
@@ -49,31 +82,32 @@ def Sperrplotter(Pfad, Label, Marker):
     plt.scatter(xdata, ydata, label=Label, marker=Marker)
     return 0
 
-Plotter("Grün_525nm_Durchlassrichtung.txt", "Grün_525nm_Sperrrichtung.txt", "Grün", "+")
-Plotter("Rot_630nm_Durchlassrichtung.txt", "Rot_630nm_Sperrrichtung.txt", "Rot", "v")
-Plotter("WeißesLicht_60W_Durchlassrichtung.txt", "WeißesLicht_60W_Sperrrichtung.txt", "Weiß", "D")
-Plotter("Tageslicht_Durchlassrichtung.txt", "Tageslicht_Sperrrichtung.txt", "Tageslicht", "x")
-plt.legend(loc = "upper left" , fancybox = True , frameon = True , edgecolor = "black")
+#Plotter("Grün_525nm_Durchlassrichtung.txt", "Grün_525nm_Sperrrichtung.txt", "Grün", "+")
+#Plotter("Rot_630nm_Durchlassrichtung.txt", "Rot_630nm_Sperrrichtung.txt", "Rot", "v")
+#Plotter("WeißesLicht_60W_Durchlassrichtung.txt", "WeißesLicht_60W_Sperrrichtung.txt", "Weiß", "D")
+#Plotter("Tageslicht_Durchlassrichtung.txt", "Tageslicht_Sperrrichtung.txt", "Tageslicht", "x")
+#plt.legend(loc = "upper left" , fancybox = True , frameon = True , edgecolor = "black")
 plt.grid()
-plt.show()
+#plt.show()
 
-Sperrplotter("Grün_525nm_Sperrrichtung.txt", "Grün", "+")
-Sperrplotter("Rot_630nm_Sperrrichtung.txt", "Rot", "v")
-Sperrplotter("WeißesLicht_60W_Sperrrichtung.txt", "Weiß", "D")
-Sperrplotter("Tageslicht_Sperrrichtung.txt",  "Tageslicht", "x")
-plt.legend(loc = "center left" , fancybox = True , frameon = True , edgecolor = "black")
+#Sperrplotter("Grün_525nm_Sperrrichtung.txt", "Grün", "+")
+#Sperrplotter("Rot_630nm_Sperrrichtung.txt", "Rot", "v")
+#Sperrplotter("WeißesLicht_60W_Sperrrichtung.txt", "Weiß", "D")
+#Sperrplotter("Tageslicht_Sperrrichtung.txt",  "Tageslicht", "x")
+#plt.legend(loc = "center left" , fancybox = True , frameon = True , edgecolor = "black")
 plt.grid()
-plt.show()
+#plt.show()
 # plt.legend(loc = "upper right" , fancybox = True , frameon = True , edgecolor = "black")
-lnPlotter("Grün_525nm_Durchlassrichtung.txt", "Grün_525nm_Sperrrichtung.txt", "Grün", "+")
+#lnPlotter("Grün_525nm_Durchlassrichtung.txt", "Grün_525nm_Sperrrichtung.txt", "Grün", "+")
 
-plt.legend(loc = "upper left" , fancybox = True , frameon = True , edgecolor = "black")
-plt.grid()
-plt.show()
+#plt.legend(loc = "upper left" , fancybox = True , frameon = True , edgecolor = "black")
+#plt.grid()
+#plt.show()
 #plt.scatter(xgreenzip, ygreenzip)
 #plt.scatter(xrotzip, yrotzip)
 #plt.show()
 
 
-print(popt)
-plt.show()
+
+#plt.show()
+
